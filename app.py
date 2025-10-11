1635aad6a2558fe12de0f7253320f5a446db2320
Eijis12 <anferneedevilla3@gmail.com>
2025-10-11 17:02:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -164,4 +164,7 @@ def appointments():
 # ---------- Run App ----------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
