f0b177104b046ff6f8f149df6e26cc00f38fcfdd
Eijis12 <anferneedevilla3@gmail.com>
2025-10-15 19:48:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -174,5 +174,6 @@ def download_forecast():
 # Run App (Render-compatible)
 # ==========================================================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
+
+
