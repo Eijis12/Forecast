47b0aa9184feb519f5931640bf7a21a0df899478
Eijis12 <anferneedevilla3@gmail.com>
2025-10-15 19:03:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,7 @@ def home():
 # Production entry point
 # -------------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
