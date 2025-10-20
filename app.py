c8b38d1068545b33a37d67a9ed25c9010f48ac12
Eijis12 <anferneedevilla3@gmail.com>
2025-10-20 18:36:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,9 @@ def index():
     return "Dental Revenue Forecasting API Running (Hybrid Model)"
 
 
-# ===============================
-# Main
-# ===============================
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+
